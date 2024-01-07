06513ed30bdd3cad8de17344b04ef7c7496625e4
Adithya Jannu <adijannu09@gmail.com>
2024-01-07 15:40:25 +0000

    container file configured

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+// Datatypes Conversion
+
+// let score = "abc"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// NOTES
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+console.log(isLoggedIn);
