a1683e91605885ce8da4b846e23ba9ed8aee2317
Adithya Jannu <adijannu09@gmail.com>
2024-01-08 16:22:29 +0530

    02_datatypes.js

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,13 +1,13 @@
 // Datatypes Conversion
 
-// let score = "abc"
+let score = "abc"
 
-// console.log(typeof score)
-// console.log(typeof(score))
+console.log(typeof score)
+console.log(typeof(score))
 
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber)
-// console.log(valueInNumber);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
 
 // NOTES
 
